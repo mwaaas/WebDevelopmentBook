bf6b7c00268887cc9c12831522659ae522eb9b69
isaackaranja <37794994+isaackaranja@users.noreply.github.com>
2022-04-01 11:39:52 +0300

    fixing unit tests (#1)
    * fixing unit tests
    
    * initial implementation

--- BookReviewFromScratch/without_database/user.py
+++ BookReviewFromScratch/without_database/user.py
@@ -1,11 +1,10 @@
 from typing import List
+from datetime import datetime
 
 # This will act as our store for a start we will later convert it to something that can be persisted. 
 # the structure of this should end up with something like this 
 # {"john": {"password": "mypassword", "dateOfBirth": "30/12/1980"}}
-users = {}
-
-
+users = {"john": {"password": "mypassword", "dateOfBirth": "30/12/1980"}, "foo": {"password":"bar", "dateOfBirth": "30/12/1980"}}
 
 
 # exception throw when username does not exist 
@@ -30,27 +29,52 @@ class InvalidDateOfBirth(Exception):
     pass 
 
 
-def login(username: string, password: string) -> {}:
-    """ 
-    Returns user details if user exist else returns None
-    """
-    pass
+def login(username: str, password: str) -> {}:      
+  """ 
+  Returns user details if user exist else returns None
+  """
+  user = users.get(username)
+  if user:
+    if user.get('password') != password:
+      raise InvalidPassword("wrong password")
+    else:
+      new_user = user.copy()
+      new_user['username'] = username
+      return new_user
+  else:
+    raise InvalidUsername("")
 
+def register(username: str, password: str, password2: str, dateOfBirth: str):
+  """
+  if user exist return error saying user already exist 
 
-def register(username: string, password: string, password2: string, dateOfBirth: string):
-    """
-    if user exist return error saying user already exist 
+  if password1 and password2 are equal and username does not exist it will create 
+  a new user on our user dictionary and return user. 
 
-    if password1 and password2 are equal and username does not exist it will create 
-    a new user on our user dictionary and return user. 
-
-    if password1 and password2 is not equal raise an error.  
-    """
-    pass
-
+  if password1 and password2 is not equal raise an error.  
+  """
+  try:
+    type_date_type = type(datetime.strptime(dateOfBirth, '%d/%m/%Y'))
+  except ValueError:
+    raise InvalidDateOfBirth(Exception)
+  if password != password2:
+    raise PasswordMissmatch("your passwords dont match")
+  if users.get('username'):
+    raise UserAlreadyExist('user already exits')
+  else:
+    users['username'] = {'username': username, "password": password, "dateOfBirth": dateOfBirth}
+    return {"username": username, "password": password, "dateOfBirth": dateOfBirth}
 
 def list_users() -> List:
     pass 
 
 if __name__ == '__main__':
-    login("foo", "mysecret_password")
+  # print("register:", register("foo", "password", "password", ""))
+
+  print("login:", login("john", "mypassword"))
+
+  
+  print("login:", login("foo", "mysecret_password"))
+
+  print("continuing")
+              
