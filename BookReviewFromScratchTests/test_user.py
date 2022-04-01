d97cba1ec52e1ad65a0b0abc042d352d71a7ab7e
Francis Mwangi <francismwangi152@gmail.com>
2022-04-01 09:09:04 +0000

    refactoring test to be separated

--- BookReviewFromScratchTests/test_user.py
+++ BookReviewFromScratchTests/test_user.py
@@ -5,24 +5,13 @@ from datetime import datetime
 
 class TestUser(TestCase):
 
-    def test(self):
+    def test_register(self):
         registered_user = user.register("foo", "bar", "bar", "30/12/1980")
 
         self.assertDictEqual(registered_user,  {"username": "foo", "password": "bar", "dateOfBirth": "30/12/1980"})
-        
-        self.assertEqual(registered_user, user.login("foo", "bar"))
-
-
-        # test login with invalid password
-        self.assertRaises(user.InvalidPassword, user.login, "foo", "wrongPassword")
-
-        # test login with user who does not exist 
-        self.assertRaises(user.InvalidUsername, user.login, "bar",  "bar")
-
 
         # test register with user name who already exist 
         self.assertRaises(user.UserAlreadyExist, user.register, "foo", "password", "password", "30/12/1980")
-
 
         # test register user with password missmatch 
         self.assertRaises(user.PasswordMissmatch, user.register, "favw", "password", "passwOrd", "30/12/1980")
@@ -30,4 +19,13 @@ class TestUser(TestCase):
         # test invalid date of birth 
         self.assertRaises(user.InvalidDateOfBirth, user.register, "favw", "password", "password", "invalid_date")
 
+    def test_login(self):
+        # first register user
+        registered_user = user.register("user1", "user1password", "user1password", "30/12/1980")
+        self.assertEqual(registered_user, user.login("user1", "user1password"))
 
+        # test login with invalid password
+        self.assertRaises(user.InvalidPassword, user.login, "user1", "wrongPassword")
+
+        # test login with user who does not exist
+        self.assertRaises(user.InvalidUsername, user.login, "bar",  "bar")
