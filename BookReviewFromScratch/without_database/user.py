f1e7945eae67ca33f804d1b5884ad213cf4ee557
Francis Mwangi <francismwangi152@gmail.com>
2022-04-01 09:07:03 +0000

    remove implementation

--- BookReviewFromScratch/without_database/user.py
+++ BookReviewFromScratch/without_database/user.py
@@ -4,7 +4,7 @@ from datetime import datetime
 # This will act as our store for a start we will later convert it to something that can be persisted. 
 # the structure of this should end up with something like this 
 # {"john": {"password": "mypassword", "dateOfBirth": "30/12/1980"}}
-users = {"john": {"password": "mypassword", "dateOfBirth": "30/12/1980"}, "foo": {"password":"bar", "dateOfBirth": "30/12/1980"}}
+users = {}
 
 
 # exception throw when username does not exist 
@@ -33,16 +33,7 @@ def login(username: str, password: str) -> {}:
   """ 
   Returns user details if user exist else returns None
   """
-  user = users.get(username)
-  if user:
-    if user.get('password') != password:
-      raise InvalidPassword("wrong password")
-    else:
-      new_user = user.copy()
-      new_user['username'] = username
-      return new_user
-  else:
-    raise InvalidUsername("")
+  pass
 
 def register(username: str, password: str, password2: str, dateOfBirth: str):
   """
@@ -53,22 +44,13 @@ def register(username: str, password: str, password2: str, dateOfBirth: str):
 
   if password1 and password2 is not equal raise an error.  
   """
-  try:
-    type_date_type = type(datetime.strptime(dateOfBirth, '%d/%m/%Y'))
-  except ValueError:
-    raise InvalidDateOfBirth(Exception)
-  if password != password2:
-    raise PasswordMissmatch("your passwords dont match")
-  if users.get('username'):
-    raise UserAlreadyExist('user already exits')
-  else:
-    users['username'] = {'username': username, "password": password, "dateOfBirth": dateOfBirth}
-    return {"username": username, "password": password, "dateOfBirth": dateOfBirth}
+  pass
 
 def list_users() -> List:
     pass 
 
 if __name__ == '__main__':
+  " Write logs here to test if its working"
   # print("register:", register("foo", "password", "password", ""))
 
   print("login:", login("john", "mypassword"))
