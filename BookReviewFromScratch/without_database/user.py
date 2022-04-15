7c55ce9c3ebc1fff58dd747621e5fe2cfd68d793
Francis Mwangi <francismwangi152@gmail.com>
2022-04-15 14:43:41 +0000

    update user file

--- BookReviewFromScratch/without_database/user.py
+++ BookReviewFromScratch/without_database/user.py
@@ -1,11 +1,6 @@
 from typing import List
 from datetime import datetime
 
-# This will act as our store for a start we will later convert it to something that can be persisted. 
-# the structure of this should end up with something like this 
-# {"john": {"password": "mypassword", "dateOfBirth": "30/12/1980"}}
-users = {}
-
 
 # exception throw when username does not exist 
 class InvalidUsername(Exception):
@@ -28,35 +23,29 @@ class PasswordMissmatch(Exception):
 class InvalidDateOfBirth(Exception):
     pass 
 
+def clear_users():
+    """ only used when running tests"""
+    pass
 
 def login(username: str, password: str) -> dict:
-  """ 
-  Returns user details if user exist else returns None
-  """
-  pass
+    """ 
+        Returns user details if user exist else returns None
+    """
+    pass
 
 def register(username: str, password: str, password2: str, dateOfBirth: str):
-  """
-  if user exist return error saying user already exist 
+    """
+    if user exist return error saying user already exist
 
-  if password1 and password2 are equal and username does not exist it will create 
-  a new user on our user dictionary and return user. 
+    if password1 and password2 are equal and username does not exist it will create
+    a new user on our user dictionary and return user.
 
-  if password1 and password2 is not equal raise an error.  
-  """
-  pass
+    if password1 and password2 is not equal raise an error.
+    """
+    pass
 
 def list_users() -> List:
     pass 
 
 if __name__ == '__main__':
-  " Write logs here to test if its working"
-  # print("register:", register("foo", "password", "password", ""))
-
-  print("login:", login("john", "mypassword"))
-
-  
-  print("login:", login("foo", "mysecret_password"))
-
-  print("continuing")
-              
+    """ You can put your testing logic here """
